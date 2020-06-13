27101a33410012f6e9199fdee3c72e575cf6afca
Ubaid-arif <giftprintingservice@gmail.com>
2020-06-14 02:34:03 +0500

    first commit

---
+++ array.js
@@ -0,0 +1,468 @@
+// // chapter 1 //
+
+
+// // alert ("Error! please enter a valid password "); 
+// // alert ("welcome to js land... \n happy coding! ");
+// //  alert ("welcome to js land ");
+// //   alert ("happy coding! \n prevent this page from creating additional dialogs.");
+
+// // chapter 2//
+
+
+// // var hello = "Hello world"   
+// // alert(hello); 
+
+// //   var myName = "Zain"   
+// // alert(myName);
+
+// // var age = "19"   
+// // alert(age);
+
+// // var course = "certified mobile devrlopment"   
+// // alert(course);
+
+// // var hello1 = "pizza \n pizz \n piz \n pi \n p"   
+// // alert(hello1);
+
+// // var Email = "my email is example @example.com"   
+// // alert(Email);
+
+// // var text = "i am try to learn from the book a smarter way to learn javascript "   
+// // alert(text);
+
+// // document.write("Yah! I can write Html content through JavaScript");
+
+
+// // var de = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// // alert(de);
+
+// // document.write(hello )
+
+
+
+// // chapter 3  //
+
+
+// var age = "i am 19 year old"
+// alert(age );
+
+
+// var visit = "you have visited this site 14 times"
+// alert(visit);
+
+// var order = 5
+
+// var age = 1990;
+// document.write(" My birth year is" + age + "<br/>" + "John Doe  ordered " + order +  "t-shirt(s) on XYZ cloting store   ")
+
+
+
+// //chapter 4 //
+
+
+
+
+// // var name = "sheryar"
+// // var course = "web and mob"
+// // var batch = "5"
+// // alert( name + course + batch   )   //+" <br/>"
+
+// // var dash = "_"
+// // var dollar = "$"
+// // var sense = "sensitive"
+// // var key ="keyword"
+
+// // document.write("<h1>" +" rules for js variable " + "</h1>"    )
+// // document.write(  "Variable names can only contain" + dollar   +" and "+ dash+ "<br/>"  )
+// // document.write(  " Variables must begin with a" + dollar  + "and " +dash + "For example $name, _name or name " + "<br/>" )
+// // document.write(  "Variable names are case" + sense + "<br/>"  )
+// // document.write(  "Variable names should not be JS"+ key  + "<br/>"  )
+
+
+
+// // chapter  5//
+
+
+
+// // document.write( "sum of 3 and 5 is " +  (3+5) + "<br/>"   )
+// // document.write( "subtract of 3 and 5 is " +  (3-5) + "<br/>"   )
+// // document.write( "mutiply of 3 and 5 is " +  (3*5) + "<br/>"   )
+// // document.write( "divide of 3 and 5 is " +  (3/5) + "<br/>"   )
+// // document.write( "sum of 3 and 5 is " +  (5%3) + "<br/>"   )
+
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+
+// // var number = "5"
+
+// // document.write( "initial value is : " +  number + "<br/>"   )
+// // document.write( "value after increment is : " +  ( ++number) + "<br/>"   )
+// // document.write( "adding value of 6 is  " +  (number + 7 ) + "<br/>"   )
+// // var number =  (7+ number) 
+// // document.write( "value after decrement is : " + (--number) + "<br/>"   )
+// // document.write( "reminder after dividing by 3 : " +  (number%3) + "<br/>"   )
+
+
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+// // var cost = "600"
+
+
+
+// // document.write("<b>" + " Cost of one movie ticket is :" + "</b>" + 600 + "pkr  "  +   "<br/>"   )
+
+// // document.write( "cost of 5 ticket is:  " +  (cost*5) + "<br/>"   )
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+
+// // var table = "4"
+// // var count = "0"
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+// // document.write(table + " X " + ++count + "= " + (table*count) + "<br />")
+
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+// // document.write( "convert  fahrenhiet into celcius "  + "<br/>"   )
+
+// // var f = "70"
+
+// // document.write( "C" + " = "  + " (f - 32 X 5/9) " +  " = " + ( f - 32 * 5/9 )  + "<br/>"   )
+
+
+// // document.write(  "<br/>"   )
+// // document.write( "convert celcius into fahrenhiet "  + "<br/>"   )
+
+// // var c = "25"
+
+// // document.write( "f"  + " = " + "(c X 9/5 + 32) " + " = "  + (  c*9/5+32  ) +  "<br/>"   )
+
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+// // var item1 = "650"
+// // var item2 = "100" 
+// // document.write(  " price of 1 item is: " + item1 +    "<br/>"   )
+// // document.write(  "quantity of 1 item is : 3 " +  "<br/>"   )
+
+// // document.write(  " price of 2 item is: " + item2 +    "<br/>"   )
+
+// // document.write(  "quantity of 2 item is : 7 "  + "<br/>"   )
+// // document.write(  "the shipping charges  is : 100 " +  "<br/>"   )
+// // var shipping = "100"
+// // document.write(  "the total cost of your ordered  is : " +  (item1*3+item2*7+100) +"<br/>" )
+ 
+
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+// // var total = "980 "
+// // var obtain = "804"
+// // document.write( "total marks :" + total      +"<br/>"   )
+// // document.write(  "marks obtain :" + obtain +  "<br/>"   )
+// // document.write( "percentage : " + ( obtain*100/total  ) +"<br/>"   )
+
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+// // var rayal = "28"
+// // var us = "104.80"
+// // document.write( " <h1> " +" currency convert " + "</h1>" + "<br/>"   )
+// // document.write( "1 saudi rayal is : " + rayal + "rupee" +"<br/>"   )
+// // document.write( "1 us dollar is :"+ us +" rupee " +"<br/>"   )
+
+
+// // document.write( "total currency in PKR is:"+  (rayal*25+us*10) +"<br/>"   )
+
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+// // document.write( "<b>"+" we suppose 5 Add 5 Multiply by 10 Divide the result by 2 " +"</b>" + (5+5*10/2)  + "<br/>"   )
+
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+// // document.write(  "<br/>"   )
+// // document.write( "<h1>" + "the age calculator" + "</h1>" +"<br/>"   )
+
+
+// // var current = "2020"
+// // var birth = "1999"
+// // document.write( "current year :"+current +"<br/>"   )
+// // document.write("birth year :" + birth   +"<br/>"   )
+
+
+// // document.write("your age is :"+ (current-birth  )  + "<br/>"   )
+// // document.write(  "<br/>"   )
+
+// // document.write(  "<br/>"   )
+
+// // document.write(  "<br/>"   )
+
+// // var radius = "20"
+// // document.write( "<h1>" + "geomatry" + "</h1>" + "<br/>"   )
+// // document.write( "the radius of a circle :" +(radius) + "<br/>"   )
+
+
+// // document.write( "the cicumference of a circle :" + (2*3.142*radius )  +"<br/>"   )
+// // document.write( "the area is:" + (3.142*radius*radius) + "<br/>"   )
+
+// // document.write(  "<br/>"   )
+// // document.write(  "<br/>"   )
+
+// // document.write( "<h1>"+ "life time supply calculator" + "</h1>"  + "<br/>"   )
+// // var age = "15"
+// // document.write( "your current age : " +(age) + "<br/>"   )
+
+// // document.write( "your favourite snake : choclate chip "+ "<br/>"   )
+// // var max = "65"
+// // document.write("your maximum age :" + (max) + "<br/>"   )
+// // var day = "3"
+// // document.write("you eat per day " +(day) + "<br/>"   )
+// // document.write( "you will need " + (max-age)*day + " choclate chip to last you until the rip old age of " + (max) + "<br/>"   )
+
+ 
+
+// // chapter 6 - 9 tak 
+
+
+
+
+// // var a = ("10") 
+
+// // document.write ( "the value of A is  " + (a) + "<br/>" ) 
+// // document.write ( "the value of ++A is  " + (++a) + "<br/>" ) 
+
+// // document.write ( " now the value of A is  " + (a) + "<br/>" ) 
+
+// // document.write ( "the value of A++ is  " + (a++) + "<br/>"  ) 
+
+// // document.write ( "the value of A is  " + (a)  + "<br/>" ) 
+
+// // document.write ( "the value of --A is  " + (--a)  + "<br/>" ) 
+
+// // document.write ( "the value of A is  " + (a)  + "<br/>" ) 
+
+// // document.write ( "the value of A-- is  " + (a--)  + "<br/>" ) 
+
+// // document.write ( "the value of A is  " + (a)  + "<br/>" ) 
+
+
+
+// // var a = ("2")
+// // var b = ("1")
+
+// // document.write ( " --a ; " + (--a)  + "<br/>" ) 
+
+// // document.write ( " --a - --b; " + (--a - --b)  + "<br/>" ) 
+
+// // document.write ( " --a - --b + ++b; " + (--a - --b + ++b)   + "<br/>" ) 
+
+// // document.write ( " --a - --b + ++b + b-- ; " + (--a - --b + ++b + b--)  + "<br/>" ) 
+
+
+// // document.write ( " a ; " + (a)  + "<br/>" ) 
+// // document.write ( " b ; " + (b)  + "<br/>" ) 
+// // document.write ( " the result is ;" + ((--a - --b) + (++b + b--))  + "<br/>" ) 
+
+
+// // var name = prompt("enter your name")
+
+// // var number = +prompt("enter your number")
+
+// //      if(number <= '0') {
+// //              number = 5 };
+
+
+// // var count = 0
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+// // document.write(number + " X " + ++count + "= " + (number*count) + "<br />")
+
+
+
+
+// // document.write("---------------------Marksheet-----------------" + "<br />")
+
+// // var subjectName1 = prompt("Enter Your Subject Name?")
+// // var subjectMarks1 = +prompt("Enter your " + subjectName1 + " Marks" )
+// // var subjectName2 = prompt("Enter Your Subject Name?")
+// // var subjectMarks2 = +prompt("Enter your " + subjectName2 + " Marks" )
+// // var subjectName3 = prompt("Enter Your Subject Name?")
+// // var subjectMarks3 = +prompt("Enter your " + subjectName3 + " Marks" )
+
+// // var obtainedMarks= subjectMarks1 +subjectMarks2+ subjectMarks3 
+// // var totalsMarks= 300
+
+// // document.write("Subject Name is: " + subjectName1 + "<br />")
+// // document.write("Subject Marks is: " + subjectMarks1 + "<br />")
+// // document.write("Percentage% of " + subjectName1 + " from " + totalsMarks + " is " + (subjectMarks1/totalsMarks*100) + "<br />")
+
+// // document.write("Subject Name is: " + subjectName2 + "<br />")
+// // document.write("Subject Marks is: " + subjectMarks2 + "<br />")
+
+// // document.write("Subject Name is: " + subjectName3 + "<br />")
+// // document.write("Subject Marks is: " + subjectMarks3 + "<br />")
+
+// // document.write("Totals Marks Achieved is: " + obtainedMarks)
+
+
+
+// // chapter 9 - 11 //
+
+
+
+// // var city = prompt("enter your city name")
+// //  if(city === "karachi"    ){
+// //      alert("welcome to city of light")
+// //  }
+
+// // var gender = prompt("enter your gender")
+// // if(gender === "male"){
+// //     alert("Good Morning Sir")
+// // }
+// // else if (gender === "female"){
+// //     alert("Good morning madam")
+// // }
+
+// // var signal = prompt("enter traffic signal color")
+// // if(signal === "green"){
+// //     alert("Move now")}
+// // else if (signal === "red"){
+// //     alert("must stop")
+// // }
+// // else if (signal === "yellow"){
+// //     alert("Ready to move")
+// // }
+
+// // var fuel = +prompt("enter remaining fuel in your car ")
+ 
+// // if(fuel <= 0.25  ){
+// //     alert(" “Please refill the fuel in your car” ")}
+
+
+// // var a = 4;
+// // if (++a === 5){
+// // alert("given condition for variable a is true");
+// // }
+
+// // var b = 82;
+// // if (b++ === 83){
+// // alert("given condition for variable b is false");
+// // }
+
+// // var c = 12;
+// // if (c++ === 13){
+// // alert("condition 1 is false");
+// // }
+// // if (c === 13){
+// // alert("condition 2 is true");
+// // }
+// // if (++c < 14){
+// // alert("condition 3 is false");
+// // }
+// // if(c === 14){
+// // alert("condition 4 is true");
+// // }
+// // ("car" < "cat"){
+// //     alert("car is smaller than cat");
+// //  }
+
+
+
+// // var temp = +prompt("enter your body temperature")
+// // if(temp >= "40"){
+// //     alert("its too hot outside ")}
+// // else if (temp >= "30"){
+// //     alert("the weather today is normal")
+// // }
+// // else if (temp >= "20"){
+// //     alert("today weather is cool ")
+// // }
+// // else if (temp >= "10"){
+// //     alert("omg todays weather is so cool ")
+// // }
+
+
+
+// //   chapter 12 - 15   //
+
+
+
+// // string "" || ''
+// // number 12345
+// // boolean 
+// // array []
+// // var std1 = 'Studen 1';
+// // var std2 = 'Studen 2';
+// // var std3 = 'Studen 3';
+
+// // alert("Welcome to " + std1);
+// // index == 0
+// //  var students = [ "Student 1", "Student 2", "Student 3" ]
+// //  console.log(students[0])
+// // console.log(students[1])
+// // console.log(students[2]) 
+// // students = ["std 1", "std 2"]
+// // alert("Welcome to " + students[0])
+
+
+// // var color = ["orange"]; 
+// // pets[1] = "black" 
+// //  alert(pets); 
+// // pets[10] = "pink" 
+// // pets[5] = "blue";
+// // console.log(pets[0]);
+// // console.log(pets[10]);
+// // console.log(pets[5]);
+// // var color = ['orange ', 'yellow ' , 'blue' , 'purple', 'red' ,'green', 'grey'];
+// // color.shift('violet');
+// // console.log(color);
+// // color.unshift('deer', 'snake','monkey');
+// // color.splice(2 , 1) 
+// // var removecolor = color.splice(2, 1)
+// // color.push("abc"); 
+// // color.push("abc"); 
+// // var lastElement = color.push("abc"); 
+// // console.log(color);
+
+// // console.log(lastElement)
+// // console.log(removecolor);
+// // var copyPets = color.slice(2);
+// // console.log(copyPets)
+// // colr.push('dark')
+// // console.log(color);
